2b631f80ef579ea0bbe1db9821a09b4708a0fd7d
Robert Toroitich <robertojnr2802@gmail.com>
2026-07-03 12:35:51 +0300

    Added route and code for get all items

--- app/routes.py
+++ app/routes.py
@@ -8,3 +8,7 @@ inventory_bp = Blueprint("inventory", __name__)
 # POST /inventory → Add a new item
 # PATCH /inventory/<id> → Update an item
 # DELETE /inventory/<id> → Remove an item
+
+@inventory_bp.route("/", methods=["GET"]):
+def get_all_items():
+    return jsonify(inventory), 200
