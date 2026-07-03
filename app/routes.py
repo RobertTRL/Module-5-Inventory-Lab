cff450d8e0a7e37d72b15e85a19849aca3ed1ebd
Robert Toroitich <robertojnr2802@gmail.com>
2026-07-03 12:41:48 +0300

    Added get item by id code

--- app/routes.py
+++ app/routes.py
@@ -14,3 +14,10 @@ def get_all_items():
     return jsonify(data.inventory), 200
 
 @inventory_bp.route("/<int:item_id>", methods=["GET"])
+def get_item_by_id(item_id):
+    item = next((i for i in data.inventory if i.id == item_id), None)
+
+    if not item:
+        return jsonify({"error": "Item not found"}), 404
+    
+    return jsonify(item), 200
