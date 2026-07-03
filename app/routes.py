10f3846aa7b6958d41d53fe003fbbc73e45cda1e
Robert Toroitich <robertojnr2802@gmail.com>
2026-07-03 12:37:22 +0300

    Removed colon in route decorator line in routes

--- app/routes.py
+++ app/routes.py
@@ -9,6 +9,6 @@ inventory_bp = Blueprint("inventory", __name__)
 # PATCH /inventory/<id> → Update an item
 # DELETE /inventory/<id> → Remove an item
 
-@inventory_bp.route("/", methods=["GET"]):
+@inventory_bp.route("/", methods=["GET"])
 def get_all_items():
     return jsonify(inventory), 200
