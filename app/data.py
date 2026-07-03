66c47cda35c690c650343359ddccad1a2dd6f091
Robert Toroitich <robertojnr2802@gmail.com>
2026-07-03 12:12:12 +0300

    Added inventory list with 2 dicts as items in the list

---
+++ app/data.py
@@ -0,0 +1,22 @@
+inventory = [
+    {
+        "id": 1,
+        "product_name": "Organic Almond Milk",
+        "brands": "Silk",
+        "barcode": "0025293001453",
+        "ingredients_text": "Filtered water, almonds, cane sugar, sea salt, ...",
+        "price": 4.99,
+        "stock": 20,
+        "source": "manual"
+    },
+    {
+        "id": 2,
+        "product_name": "Peanut Butter",
+        "brands": "Jif",
+        "barcode": "0051500255162",
+        "ingredients_text": "Roasted peanuts, sugar, molasses, salt",
+        "price": 3.49,
+        "stock": 15,
+        "source": "manual"
+    }
+]
